435fff8d51a0ab675ecc353c5b40849e0ff000ae
sbx_user1051 <sbx_user1051@169.254.39.29>
2019-11-04 14:14:23 +0000

    Mon Nov 04 14:14:23 UTC 2019

--- src/main/java/befaster/solutions/CHL/CheckliteSolution.java
+++ src/main/java/befaster/solutions/CHL/CheckliteSolution.java
@@ -1,9 +1,33 @@
 package befaster.solutions.CHL;
 
-import befaster.runner.SolutionNotImplementedException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class CheckliteSolution {
+
+    private final static Map<String, Integer> pricesPerSKU = loadPrices();
+
     public Integer checklite(String skus) {
-        throw new SolutionNotImplementedException();
+        return pricesPerSKU.get(skus);
+    }
+
+    /*
+     +------+-------+----------------+
+    | Item | Price | Special offers |
+    +------+-------+----------------+
+    | A    | 50    | 3A for 130     |
+    | B    | 30    | 2B for 45      |
+    | C    | 20    |                |
+    | D    | 15    |                |
+    +------+-------+----------------+
+    */
+    private static Map<String, Integer> loadPrices() {
+        Map<String, Integer> prices = new HashMap<>();
+        prices.put("A", 50);
+        prices.put("B", 30);
+        prices.put("C", 20);
+        prices.put("D", 15);
+        return prices;
     }
 }
+
