4cf21c468ca74a4ebb250170fb681284731320b0
sbx_user1051 <sbx_user1051@169.254.39.29>
2019-11-04 14:47:24 +0000

    Mon Nov 04 14:47:24 UTC 2019

--- src/main/java/befaster/solutions/CHL/CheckliteSolution.java
+++ src/main/java/befaster/solutions/CHL/CheckliteSolution.java
@@ -1,31 +1,31 @@
 package befaster.solutions.CHL;
 
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.regex.Pattern;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class CheckliteSolution {
 
     private static final Map<Character, Integer> PRICES_PER_SKU = loadPrices();
-    private static final Map<Character, Integer> BUNDLE_FOR_SKU = new HashMap<>();
+    private static final Map<Character, Integer> BUNDLE_FOR_SKU = loadBundles();
+    private static final Map<Character, Integer> PRICES_PER_BUNDLE = loadPricesPerBundle();
+
     private static final Pattern ALLOWED_SKU = Pattern.compile("[ABCD]+");
 
     public Integer checklite(String skus) {
-        if(!isValidInput(skus)) {
+        if (!isValidInput(skus)) {
             return -1;
         }
 
         char[] allSKUs = skus.toCharArray();
 
         int result = 0;
-        for(char sku : allSKUs) {
-            result += PRICES_PER_SKU.get(sku)
+        for (char sku : allSKUs) {
+            result += PRICES_PER_SKU.get(sku);
         }
         return result;
     }
+
 
     /*
      +------+-------+----------------+
@@ -37,7 +37,6 @@ public class CheckliteSolution {
     | D    | 15    |                |
     +------+-------+----------------+
     */
-
     private static Map<Character, Integer> loadPrices() {
         Map<Character, Integer> prices = new HashMap<>();
         prices.put('A', 50);
@@ -47,10 +46,18 @@ public class CheckliteSolution {
         return prices;
     }
 
+    private static Map<Character, Integer> loadBundles() {
+        Map<Character, Integer> bundles = new HashMap<>();
+        bundles.put('A', 3);
+        bundles.put('B', 2);
+        return bundles;
+    }
+
     private boolean isValidInput(String skus) {
         return skus != null && ALLOWED_SKU.matcher(skus).matches();
     }
 }
+
 
 
 
