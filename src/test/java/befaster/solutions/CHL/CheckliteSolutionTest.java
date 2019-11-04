28633802f53a4fbab442cfa21c2d2996465e2677
sbx_user1051 <sbx_user1051@169.254.39.29>
2019-11-04 14:11:23 +0000

    Mon Nov 04 14:11:23 UTC 2019

--- src/test/java/befaster/solutions/CHL/CheckliteSolutionTest.java
+++ src/test/java/befaster/solutions/CHL/CheckliteSolutionTest.java
@@ -1,5 +1,20 @@
+package befaster.solutions.CHL;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.IsEqual.equalTo;
 import static org.junit.Assert.*;
 
 public class CheckliteSolutionTest {
 
+    CheckliteSolution solution = new CheckliteSolution();
+
+    @Test
+    public void checklite_processOneItem() {
+        String skus = "A";
+
+        int totalValue = solution.checklite(skus);
+
+        assertThat(totalValue, equalTo(50));
+    }
 }
